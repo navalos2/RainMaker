679b65430547ac7177cb73044b0655ee0b314ea1
navalos2 <nataly.noriega@ymail.com>
2022-12-14 17:59:38 -0800

    up and down movement

--- GameApp.java
+++ GameApp.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -17,7 +18,7 @@ import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
-import java.util.Random;
+import java.security.Key;
 
 interface Updatable {
     void update();
@@ -28,17 +29,17 @@ interface Updatable {
  the game, win/lose conditions, instantiation and links the other Game Objects.
  */
 class Game extends Pane implements Updatable {
+    Cloud cloud = new Cloud();
+
+    Pond pond = new Pond();
+
+    Helipad helipad = new Helipad();
+
+    Helicopter helicopter = new Helicopter();
+
+    int counter = 0;
+
     public Game() {
-        Cloud cloud = new Cloud();
-
-        Pond pond = new Pond();
-
-        Helipad helipad = new Helipad();
-
-        Helicopter helicopter = new Helicopter();
-
-        /*Helicopter helicopter = new Helicopter(helipad.myTranslate.getX(),
-                helipad.myTranslate.getY());*/
 
         this.getChildren().clear();
         this.getChildren().addAll(cloud, pond, helipad, helicopter);
@@ -46,19 +47,30 @@ class Game extends Pane implements Updatable {
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                update();
+                if (counter++ %2 == 0) {
+                    helicopter.update();
+                }
             }
         };
         timer.start();
     }
-
-    public void up() {
-        // using object helicopter
-
-    }
     @Override
     public void update() {
+    }
 
+    public void heliUp() {
+        helicopter.up();
+    }
+    public void heliDown() {
+        helicopter.down();
+    }
+
+    public void heliLeft() {
+        helicopter.turnLeft();
+    }
+
+    public void heliRight() {
+        helicopter.turnRight();
     }
 }
 
@@ -76,6 +88,28 @@ class GameObject extends Group {
         myScale = new Scale();
         this.getTransforms().addAll(myTranslate, myRotate, myScale);
     }
+    public void rotate(double degrees) {
+        myRotate.setAngle(degrees);
+        myRotate.setPivotX(0);
+        myRotate.setPivotY(0);
+    }
+
+    public void scale(double sx, double sy) {
+        myScale.setX(sx);
+        myScale.setY(sy);
+    }
+
+    public void translate(double tx, double ty) {
+        myTranslate.setX(tx);
+        myTranslate.setY(ty);
+    }
+
+    public void update(){
+        for(Node n : getChildren()){
+            if(n instanceof Updatable)
+                ((Updatable)n).update();
+        }
+    }
     void add(Node node) {
         this.getChildren().add(node);
     }
@@ -89,8 +123,6 @@ class Pond extends GameObject {
         pond.setFill(Color.BLUE);
         pond.setTranslateX(300);
         pond.setTranslateY(400);
-       /* pond.setTranslateX(new Random().nextDouble(GameApp.GAME_WIDTH));
-        pond.setTranslateY(new Random().nextDouble(GameApp.GAME_HEIGHT) + 100);*/
         add(pond);
     }
 }
@@ -104,8 +136,13 @@ class Cloud extends GameObject {
         cloud.setFill(Color.WHITE);
         cloud.setTranslateX(150);
         cloud.setTranslateY(600);
-        /*cloud.setTranslateX(new Random().nextDouble(GameApp.GAME_WIDTH));
-        cloud.setTranslateY(new Random().nextDouble(GameApp.GAME_HEIGHT) + 100);*/
+
+        /*double randomX = Math.random() * (GameApp.GAME_WIDTH - cloud.getRadiusX());
+        double randomY =
+                Math.random() * (GameApp.GAME_HEIGHT * 2/3 - cloud.getRadiusY());
+        cloud.setCenterX(randomX);
+        cloud.setCenterY(randomY);*/
+
         add(cloud);
     }
 }
@@ -131,35 +168,69 @@ class Helipad extends GameObject {
         ellipse.setTranslateY(75);
         add(ellipse);
 
-        //this.myTranslate.setX(350);
-        //this.myTranslate.setY(25);
     }
 
 }
 
 class Helicopter extends GameObject implements Updatable{
-    boolean upPressed;
-    boolean leftPressed;
-    boolean rightPressed;
+    double speed = 0;
+    double maxSpeed = 10;
+    int minSpeed = -2;
 
     public Helicopter() {
         super();
-        Circle body = new Circle(30, 30, 15);
-        body.setFill(Color.YELLOW);
-        body.setTranslateX(170);
-        body.setTranslateY(45);
-        add(body);
+        Circle helicopter = new Circle(30, 30, 15);
+        helicopter.setFill(Color.YELLOW);
+        //helicopter.setCenterX(200);
+        //helicopter.setCenterY(75);
+        this.translate(170, 50);
+        add(helicopter);
+
+        Line heading = new Line();
+        heading.setStartX(helicopter.getCenterX());
+        heading.setStartY(helicopter.getCenterY());
+        heading.setEndX(helicopter.getCenterX());
+        heading.setEndY(helicopter.getCenterY() + 35);
+        heading.setStroke(Color.YELLOW);
+        add(heading);
+
+
+        this.getTransforms().clear();
+        this.getTransforms().addAll(myRotate, myTranslate);
     }
 
-    public void increaseSpeed() {
-        // focus on implenting something that changes how fast the heli moves
+    public void up() {
+        // increases the speed
+        if (speed < maxSpeed){
+            speed += 0.1;
+        }
+    }
+    public void down() {
+        // decreases the speed
+        if (speed > minSpeed){
+            speed -= 0.1;
+        }
+    }
+
+    public void turnLeft() {
 
     }
 
+    public void turnRight() {
+
+    }
 
     @Override
     public void update() {
-        // actually does the movement
+    // does the movement of the helicopter
+    //myTranslate.setY(myTranslate.getY() + Math.sin(Math.toRadians(myRotate
+        // .getAngle() * speed)));
+    //myTranslate.setX(myTranslate.getX() + Math.sin(Math.toRadians(myRotate
+        // .getAngle() * speed)));
+        this.translate(myTranslate.getX(), myTranslate.getY() + speed);
+    this.getTransforms().clear();
+    this.getTransforms().addAll(myRotate, myTranslate);
+    // updates the position of a helicopter
     }
 }
 
@@ -179,30 +250,34 @@ public class GameApp extends Application {
         public static final int GAME_WIDTH = 400;
         public static final int GAME_HEIGHT = 800;
 
-        public void keyPressed(KeyEvent evt) {
-            KeyCode key = evt.getCode();
-            System.out.println("Key Pressed: " + key);  // for testing
-
-
-            if (key == KeyCode.UP) {
-                // call to a method (UP) in game
-
-            }
-        }
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            Game root = new Game();
+        Game root = new Game();
+    @Override
+        public void start(Stage primaryStage) {
 
             root.setScaleY(-1); // flipping the display (screen) to avoid mirroring
-            //root.setTranslateX(200);
-            //root.setTranslateY(-400);
+
             Scene scene = new Scene(root, GAME_WIDTH, GAME_HEIGHT);
             primaryStage.setScene(scene);
             primaryStage.setTitle("Rain Maker - Nataly Avalos");
             scene.setFill(Color.BLACK);
 
 
+            scene.setOnKeyPressed(e -> {
+                if (e.getCode() == KeyCode.UP){
+                    root.heliUp();
+                }
+                if (e.getCode() == KeyCode.DOWN){
+                    root.heliDown();
+                }
+                if (e.getCode() == KeyCode.LEFT){
+                    root.heliLeft();
+                }
+                if (e.getCode() == KeyCode.RIGHT){
+                    root.heliRight();
+                }
+        });
+
             primaryStage.show();
         }
-
+        public static void main(String[] args) {launch(args);}
 }
